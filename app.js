f879137f42ecec116c6200d9e87db98235c6144b
jingwen kuang <jwkwendy@gmail.com>
2020-07-14 23:03:44 -0700

    Add volume slider

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-console.clear();
+// console.clear();
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+let audioCtx;
 
-const audioElement = document.querySelector("audio");
-const track = audioCtx.createMediaElementSource(audioElement);
+const audioElement = document.querySelector('audio');
+let track;
 
-const playButton = document.querySelector('.radio-control-play');
+const playButton = document.getElementById("onOffSwitch");
+
 playButton.addEventListener("click", function() {
+  if (!audioCtx) { 
+    init(); 
+  }
+
   if (audioCtx.state === "suspended") {
     audioCtx.resume();
   }
@@ -22,3 +27,45 @@ playButton.addEventListener("click", function() {
   let state = this.getAttribute("aria-checked") === "true" ? true : false;
   this.setAttribute("aria-checked", state ? "false" : "true");
 }, false);
+
+//when track ends, reset to false 
+audioElement.addEventListener("ended", () => {
+  playButton.dataset.playing = "false";
+  playButton.setAttribute("aria-checked", "false");
+}, false);
+
+function init() {
+  audioCtx = new AudioContext();
+  track = audioCtx.createMediaElementSource(audioElement);
+
+  //volume control
+  const gainNode = audioCtx.createGain();
+  const volumeControl = document.getElementById("volume");
+  volumeControl.addEventListener("input", function() {
+    gainNode.gain.value = this.value;
+  }, false);
+
+  //panner control 
+  const pannerOpts = {pan: 0};
+  const panner = new StereoPannerNode(audioCtx, pannerOpts);
+  const pannerControl = document.getElementById("panner");
+  pannerControl.addEventListener("input", function() {
+    panner.pan.value = this.value;
+  }, false)
+
+  //connect volume, panner to audioCtx
+  track.connect(gainNode).connect(panner).connect(audioCtx.destination);
+}
+  //onOffButton
+  const switchButton = document.querySelector(".power-switch");
+  switchButton.addEventListener("click", function() {
+    if (this.dataset.power === "on") {
+      audioCtx.suspend();
+      this.dataset.power = "off";
+    } else if (this.dataset.power === "off") {
+      audioCtx.resume();
+      this.dataset.power = "on";
+    }
+    // this.setAttribute("aria-checked", state ? "false" : "true");
+    // console.log(audioCtx.state);
+  },false);
